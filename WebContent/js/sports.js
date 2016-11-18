5e2307d0952bae19c34543643625db726d0e4abd
JohnD23 <david9352@hotmail.com>
2016-11-18 22:05:27 +0100

    Correction p4

---
+++ WebContent/js/sports.js
@@ -0,0 +1,40 @@
+$(document).ready(
+    function() {
+
+        $("#listSports").click(
+            function(event) {
+
+                event.preventDefault();
+                $.ajax({
+                    type : "GET",
+                    url : "/Servidor/deportes",
+                    data : String,
+                    success : function(msg) {
+
+                        for (var i =  0; i <= msg.lenght; i++) {
+
+                            $("#result").addend("<div class='list-group' id='listDeportes'>"+
+                                "<a href='#' class='list-group-item active' id='itemDeporte'>"+
+                                "<a href='#' class='list-group-item active' id='itemDeporte'>"+
+                                "<div class='media col-md-3'>"+
+                                "<figure class='pull-left'>"+
+                                "<img class='media-object img-rounded img-responsive'  src="+msg.img +" alt='placehold.it/350x250' >"+
+                                "</figure>"+
+                                "</div>"+
+                                "<div class='col-md-6'>"+
+                                "<h4 class='list-group-item-heading'>"+msg.name +"</h4>"+
+                                "<p class='list-group-item-text'> "+msg.Descrip +" </p></div>"+                                
+                                "<div class='col-md-3 text-center'>"+
+                                "<button type='button' class='btn btn-default btn-lg btn-block'  id = 'bSuscribete'> Suscribete </button>"+
+                                "<h5> 14240 <small> personas </small></h5></div></a>");
+                        };
+
+                    },
+                    error : function() {
+                        ;
+                        $("#result").load("/Servidor/listSports.html");
+
+                    }
+                });
+        });
+});
