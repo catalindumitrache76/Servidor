10940c7d28bbc6b7be662514530cff068f4d1917
JohnD23 <david9352@hotmail.com>
2016-11-19 02:45:55 +0100

    Update

--- WebContent/js/load.js
+++ WebContent/js/load.js
@@ -1,4 +1,19 @@
 $(document).ready(
     function() {
     	$("#topBar").load("topBar.html");
+    	
+    	$("#creaEvent").click(
+                function(event) {
+                	
+                	$("#cEvent").load("Event.html");
+                	
+        });
+    	
+    	$("#editDat").click(
+                function(event) {
+                	
+                	$("#editData").load("personalData.html");
+                	
+        });
+    	
     });
